979bc56da9a49fc0b0b8e907671da18961f94c02
kaidenton <notnediak@gmail.com>
2024-09-18 16:50:15 -0400

    added my first and last name initials

--- Assignment1.java
+++ Assignment1.java
@@ -5,6 +5,15 @@ import java.util.Random;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    System.out.println("KK      KK       DDDDDDDDD");
+    System.out.println("KK    KK         DD      DD");
+    System.out.println("KK  KK           DD      DD");
+    System.out.println("KK KK            DD      DD");
+    System.out.println("KKKK             DD      DD");
+    System.out.println("KK KK            DD      DD");
+    System.out.println("KK  KK           DD      DD");
+    System.out.println("KK    KK         DD      DD");
+    System.out.println("KK      KK       DDDDDDDDD");
+    System.out.println(" ");
   }
 }
