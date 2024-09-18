dfaf69b8871c22d552ed8acba40c350f101ac74c
kaidenton <notnediak@gmail.com>
2024-09-18 16:52:56 -0400

    added user input for fahrenheit and converts to celsius

--- Assignment1.java
+++ Assignment1.java
@@ -15,5 +15,18 @@ public class Assignment1 {
     System.out.println("KK    KK         DD      DD");
     System.out.println("KK      KK       DDDDDDDDD");
     System.out.println(" ");
+
+    final int BASE = 32;
+    final double CONVERSION_FACTOR = 5.0 / 9.0;
+
+    int fahrenheitTemp;
+    double celsiusTemp;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter a number in Fahrenheit: ");
+    fahrenheitTemp = scan.nextInt();
+
+    celsiusTemp = (fahrenheitTemp - BASE) * CONVERSION_FACTOR;
   }
 }
